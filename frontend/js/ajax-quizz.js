7c32c0245cea83b9955834f9395671504d95b65d
Christopher Fauconnier <64019350+Pekora-sama@users.noreply.github.com>
2020-05-10 16:45:50 +0200

    Add files via upload

--- frontend/js/ajax-quizz.js
+++ frontend/js/ajax-quizz.js
@@ -1,69 +1,76 @@
 "use strict";
 
+//Lors du chargement du document, les fonctions initQst et initQCM vont se lancer.
+
 document.addEventListener('DOMContentLoaded', initQst);
 document.addEventListener('DOMContentLoaded', initQCM);
+
+/* Déclaration des variables et constantes globales */
 
 let listeQuestion ="";
 let stock = 0;
 const ID_ZONE_QUEST= "zoneQuest";
 const ID_ZONE_REP="zoneRep";
 
-function gid(id){
+function gid(id){       //fonction servant à retourner un élément par son id dans l'html via une variable passée en paramètre.
     return document.getElementById(id);
 }
 
-function initQst(){
+/*  Fonction principale au lancement de la page englobant 2 autres fonctions (remplirQst,listeQst.
+*   Elle va permettre la création de questions dans l'HTML */
+
+function initQst(){     //Requête XML employant le webservice 'quest' renvoyant les données de la table 'questions'.
     let xhr = new XMLHttpRequest();
     xhr.open("get","http://localhost:80/quest",true);
     xhr.onload = remplirQst;
     xhr.send();
 }
 
-function remplirQst(){
-    let reponse = JSON.parse(this.responseText);
+function remplirQst(){      //Sert à l'implémentation des questions créées dans 'listeQst()' dans le innerHTML de la page.
+    let reponse = JSON.parse(this.responseText);        //on convertit le résultat du xhr en objet JS.
     listeQst(reponse);
-    gid(ID_ZONE_QUEST).innerHTML = listeQuestion;
+    gid(ID_ZONE_QUEST).innerHTML = listeQuestion;       //on utilise les données de la variable 'listeQuestion' retournée auparavant.
 }
 
 function listeQst(api){
     let j=1;
-    for (let i=0;i<api.length;i++){
-        listeQuestion += "<div id=qst-"+api[i].numQst+">"+j+") "+api[i].question+"</div><br>";
-        j++;
+    for (let i=0;i<api.length;i++){     //boucle parcourant tous les résultats de l'api.
+        listeQuestion += "<div id=qst-"+api[i].numQst+">"+j+") "+api[i].question+"</div><br>";      //variable qui servira de code HTML, qu'on va incrémenter à chaque passage de la boucle afin de servir de base pour les futures questions.
+        j++;    //on incrémente j après listeQuestion pour éviter un conflit de numéro dans le code HTML.
     }
 }
 
-
-function initQCM(){
+/* Deuxième fonction lors du lancement de la page, il est important qu'elle s'exécute en seconde place.
+   Elle va employer des fonctions permettant de lier des choix qu'on va créer aux questions désormais existantes.
+ */
+function initQCM(){     //Requête XML employant le webservice 'qcm' renvoyant les données de la table 'choix'.
     let xhr = new XMLHttpRequest();
     xhr.open("get","http://localhost:80/qcm",true);
     xhr.onload = remplirChoix;
     xhr.send();
 }
 
-function remplirChoix(){
+function remplirChoix(){        //sert à modifier le innerHTML des questions créées dans initQst().
     let reponse = JSON.parse(this.responseText);
-    console.log(reponse);
-
-    listeRep(reponse);
+    listeRep(reponse);      //on appelle listeRep avec la variable reponse passée en paramètre.
     gid(ID_ZONE_REP).innerHTML = listeChoix;
 }
 
 function listeRep(api){
     for (let i=0;i<api.length;i++) { //boucle pour parcourir le tableau des choix.
-        const div = document.getElementById("qst-"+api[i].numQuestion);
+        const div = document.getElementById("qst-"+api[i].numQuestion);     //on va chercher chaque question par son Id.
         const listeChoix = "<br><input type=\"radio\" name=choix-"+api[i].numQuestion+" id="+api[i].choix+" value="+api[i].points+" required><label for="+api[i].points+" class=\"choix\">"+api[i].texte+"</label><br>";
-        div.innerHTML += listeChoix;
+        div.innerHTML += listeChoix;        //modification du innerHTML de la constante div par la constante listeChoix.
     }
 }
 
 /* Bouton de validation du formulaire */
 function valider(event){
-    event.preventDefault();
-    getChecked();
-    document.cookie += stock;
-    window.open('page?url=resultat');
-    return stock=0;
+    event.preventDefault();     //on bloque l'envoi du formulaire.
+    getChecked();       //renvoie le nombre de bonnes réponses dans la variable stock.
+    document.cookie += stock;       //on ajoute un nouveau cookie contenant la valeur de stock après le modulo 2.
+    window.open('page?url=resultat');       //ouverture dans une nouvelle fenêtre de la page html : resultat.
+    return stock=0;     //on réinitialise la valeur de stock pour éviter les erreurs lors d'un spam de validation de formulaire.
 }
 
 function getChecked(){
@@ -72,8 +79,8 @@ function getChecked(){
             stock += parseInt(document.getElementById(i).value);
         }
     }
-    document.cookie = stock;
-    if(stock % 2 === 1){        //si cette valeur est impaire, on la réduit de 1 (c'est nécessaire afin de faire fonctionner notre webservice).
+    document.cookie = stock;    //on créé un cookie conservant la valeur (de type string) de stock avant le modulo.
+    if(stock % 2 === 1){        //afin de faire fonctionner correctement notre webservice, la valeur de stock doit être paire. Si elle est impaire, on l'arrondit vers le bas.
         stock = stock - 1;
     }
     return stock;
--- frontend/js/resultat.js
+++ frontend/js/resultat.js
@@ -1,34 +1,35 @@
 "use strict";
-document.addEventListener('DOMContentLoaded', Result);
-const COOKIE = document.cookie;     //on récupère la valeur du résultat obtenu après le modulo, sur la page précédente.
-let html = "";
+document.addEventListener('DOMContentLoaded', Result);      //au chargement de la page, utilisation de la fonction Result().
+const COOKIE = document.cookie;     //on créé une constante récupérant les valeurs du cookie de la page précédente (Quizz).
+let html = "";      //variable globale qu'on déclare à vide.
 
-function Result(){
+function Result(){      //on regarde la longueur du cookie (qui est un string)
     if(COOKIE.length == 2){
-        let char = COOKIE.split('');
+        let char = COOKIE.split('');        //on créé un tableau de STRING avec les valeurs de COOKIE.
         let xhr = new XMLHttpRequest();
-        xhr.open("get","http://localhost:80/resultat?total="+char[1],true);
+        xhr.open("get","http://localhost:80/resultat?total="+char[1],true);     //on récupère la valeur obtenue après le modulo (voir ajax-quizz.js) avec char[1], char[0] étant la valeur pouvant être impaire.
         xhr.onload = remplirResult;
         xhr.send();
     }
-    else if(COOKIE.length == 4){
-        let char = COOKIE.split('');
+    else if(COOKIE.length == 4){        //si le user a obtenu 10/10, la valeur de COOKIE est celle-ci : "1010"
+        let char = COOKIE.split('');        ////on créé un tableau de STRING avec les valeurs de COOKIE.
         let xhr = new XMLHttpRequest();
-        xhr.open("get","http://localhost:80/resultat?total="+10,true);
+        xhr.open("get","http://localhost:80/resultat?total="+10,true);          //on ajoute 10 mais on aurait pu utiliser ceci: "..."+char[0]+char[1]
         xhr.onload = remplirResult;
         xhr.send();
     }
 }
 
 function remplirResult(){
-    let reponse = this.responseText;
-
-    zoneText(reponse);
+    let reponse = this.responseText;        //on transforme en texte les données reçues de la database.
+    zoneText(reponse);          //on passe la variable reponse en paramètre afin d'obtenir de l'html.
     document.getElementById("zoneResult").innerHTML = html;
     document.cookie = "";
 }
 
-function zoneText(valeur){
+function zoneText(valeur){      //c'est cette fonction qui va afficher une phrase suite à nos résultats dans l'html.
+    /*on réutilise les conditions employées dans la fonction Result().
+    * Le fonctionnement est similaire avec les char[0],etc.*/
     if(COOKIE.length == 2){
         let char = COOKIE.split('');
         html ="<p> Vous avez la note de : "+char[0]+"/10</p><p><b>"+valeur+"</p>";
@@ -36,7 +37,7 @@ function zoneText(valeur){
     }
     else if(COOKIE.length == 4){
         let char = COOKIE.split('');
-        html ="<p> Vous avez la note de : "+10+"/10</p><p><b>"+valeur+"</p>";
+        html ="<p> Vous avez la note de : "+10+"/10</p><p><b>"+valeur+"</p>";       //ici aussi, on aurait pu mettre: ...+char[0]+char[1]+..., plutôt que ...+10+....
         return html;
     }
 }
